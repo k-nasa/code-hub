2774207ff56fba848caf76bd3db2517030ed40a3
k-nasa <htilcs1115@gmail.com>
2019-08-17 11:47:14 +0900

    json keyを指定

--- backend/model/user.go
+++ backend/model/user.go
@@ -3,16 +3,16 @@ package model
 import "time"
 
 type User struct {
-	ID          int64     `db:"id"`
+	ID          int64     `db:"id" json:"id"`
 	FirebaseUID string    `db:"firebase_uid"`
-	Email       string    `db:"email"`
-	Username    string    `db:"username"`
-	IconUrl     string    `db:"icon_url"`
-	CreatedAt   time.Time `db:"created_at"`
-	UpdatedAt   time.Time `db:"updated_at"`
+	Email       string    `db:"email" json:"email"`
+	Username    string    `db:"username" json:"username"`
+	IconUrl     string    `db:"icon_url" json:"icon_url"`
+	CreatedAt   time.Time `db:"created_at" json:"created_at"`
+	UpdatedAt   time.Time `db:"updated_at" json:"updated_at"`
 }
 
 type UserCodes struct {
-	User
-	Codes []*Code
+	User  User    `json:"user"`
+	Codes []*Code `json:"codes"`
 }
